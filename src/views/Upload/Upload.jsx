68f51d900bd77c09c33a8db55bf0c549ec328c47
leekeifuture <opnoe09@gmail.com>
2020-07-19 00:37:58 +0300

    Add file uploading

--- src/views/Upload/Upload.jsx
+++ src/views/Upload/Upload.jsx
@@ -1,4 +1,6 @@
 import withStyles from '@material-ui/core/styles/withStyles'
+import axios from 'axios'
+import Button from 'components/CustomButtons/Button.jsx'
 import PropTypes from 'prop-types'
 import React from 'react'
 import dashboardStyle
@@ -7,11 +9,37 @@ import dashboardStyle
 class Upload extends React.Component {
     constructor(props) {
         super(props)
-        this.state = {}
+        this.state = {
+            selectedFile: null
+        }
+    }
+
+    onChangeHandler = event => {
+        this.setState({
+            selectedFile: event.target.files[0],
+            loaded: 0
+        })
+    }
+
+    onClickHandler = () => {
+        const data = new FormData()
+        data.append('file', this.state.selectedFile)
+
+        axios.post('https://argus-uploader-api.herokuapp.com/upload/12345', data, {})
+            .then(res => {
+                console.log(res.statusText)
+            })
     }
 
     render() {
-        return (<></>)
+        return (<>
+            <input type="file" name="file" onChange={this.onChangeHandler} />
+            <Button color="success"
+                    onClick={this.onClickHandler}
+            >
+                Upload
+            </Button>
+        </>)
     }
 }
 
